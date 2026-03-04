0b587ee0b7ab42cc79934011ca4e5c1dde6b07ae
Iva <iva.petrova@student.howest.be>
2026-03-04 15:38:31 +0100

    Fixed Cmake and added the imgui demo code

--- Minigin/Renderer.cpp
+++ Minigin/Renderer.cpp
@@ -5,38 +5,63 @@
 #include "SceneManager.h"
 #include "Texture2D.h"
 
+//imgui libs
+#include <imgui.h>
+#include <backends/imgui_impl_sdl3.h>
+#include <backends/imgui_impl_sdlrenderer3.h>
+
 void dae::Renderer::Init(SDL_Window* window)
 {
 	m_pWindow = window;
-
 	SDL_SetHint(SDL_HINT_RENDER_VSYNC, "1");
 
-#if defined(__EMSCRIPTEN__)
 	m_pRenderer = SDL_CreateRenderer(window, nullptr);
-#else
-	m_pRenderer = SDL_CreateRenderer(window, nullptr);
-#endif
-
 	if (m_pRenderer == nullptr)
 	{
 		std::cout << "Failed to create the renderer: " << SDL_GetError() << "\n";
 		throw std::runtime_error(std::string("SDL_CreateRenderer Error: ") + SDL_GetError());
 	}
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+#if __EMSCRIPTEN__
+	// For an Emscripten build we are disabling file-system access, so let's not attempt to do a fopen() of the imgui.ini file.
+	// You may manually call LoadIniSettingsFromMemory() to load settings from your own storage.
+	io.IniFilename = NULL;
+#endif
+
+	ImGui_ImplSDL3_InitForSDLRenderer(window, m_pRenderer);
+	ImGui_ImplSDLRenderer3_Init(m_pRenderer);
+
 }
 
 void dae::Renderer::Render() const
 {
+	ImGui_ImplSDLRenderer3_NewFrame();
+	ImGui_ImplSDL3_NewFrame();
+	ImGui::NewFrame();
+	ImGui::ShowDemoWindow(); // For demonstration purposes, do not keep this in your engine
+	ImGui::Render();
+
 	const auto& color = GetBackgroundColor();
 	SDL_SetRenderDrawColor(m_pRenderer, color.r, color.g, color.b, color.a);
 	SDL_RenderClear(m_pRenderer);
 
 	SceneManager::GetInstance().Render();
 
+	ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), m_pRenderer);
 	SDL_RenderPresent(m_pRenderer);
 }
 
 void dae::Renderer::Destroy()
 {
+	ImGui_ImplSDLRenderer3_Shutdown();
+	ImGui_ImplSDL3_Shutdown();
+	ImGui::DestroyContext();
+
 	if (m_pRenderer != nullptr)
 	{
 		SDL_DestroyRenderer(m_pRenderer);
