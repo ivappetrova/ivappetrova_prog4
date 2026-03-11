9b253afa71abaa79fd233bf96644611d8c57a9db
Iva <iva.petrova@student.howest.be>
2026-03-11 22:20:44 +0100

    Fixed emscripten error

--- Minigin/Renderer.cpp
+++ Minigin/Renderer.cpp
@@ -20,7 +20,7 @@ void dae::Renderer::Init(SDL_Window* window)
 #if __EMSCRIPTEN__
 	// For an Emscripten build we are disabling file-system access, so let's not attempt to do a fopen() of the imgui.ini file.
 	// You may manually call LoadIniSettingsFromMemory() to load settings from your own storage.
-	io.IniFilename = NULL;
+	//io.IniFilename = NULL;
 #endif
 
 }
